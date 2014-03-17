1a798644ad704a0514d25d6d57978a500b536bed
Cwplus Lab <khalid.elhakym@gmail.com>
2014-03-17 14:59:52 +0100

    Update Webthumbnail.php

--- src/Webthumbnail/Lib/Webthumbnail.php
+++ src/Webthumbnail/Lib/Webthumbnail.php
@@ -1,11 +1,62 @@
 <?php
 
+/**
+* Webthumbnail
+* @author El Hakym Khalid <khalid.elhakym@gmail.com>
+*/
+
+/**
+ * =============================================================================
+ * 
+ * @file        webthumbnail.php
+ *              Modified by El Hakym Khalid <khalid.elhakym@gmail.com>
+ *             
+ * 
+ * @desc        http://webthumbnail.org
+ *              The Webthumbnail.org is a free webapi for capturing website 
+ *              screenshots in real browsers. Ready to serve thousands of 
+ *              thumbnails, fully scalable, cloud based!
+ *              Visit http://webthumbnail.org for more information.
+ *              
+ * @copyright   Copyright (C) 2012 HellWorx Software, Lukasz Cepowski
+ *              All rights reserved.
+ *              www.hellworx.com
+ *              
+ * @license     Redistribution and use in source and binary forms, with or 
+ *              without modification, are permitted provided that the following 
+ *              conditions are met:
+ *              
+ *              - Redistributions of source code must retain the above copyright 
+ *                notice, this list of conditions and the following disclaimer.
+ *              
+ *              - Redistributions in binary form must reproduce the above 
+ *                copyright notice, this list of conditions and the following 
+ *                disclaimer in the documentation and/or other materials 
+ *                provided with the distribution.
+ *                
+ *              THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND 
+ *              CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, 
+ *              INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF 
+ *              MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+ *              DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR 
+ *              CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+ *              SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT 
+ *              NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
+ *              LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) 
+ *              HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+ *              CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR 
+ *              OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, 
+ *              EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *              
+ * =============================================================================
+ */
+
 namespace Webthumbnail\Lib;
 
 use Webthumbnail\Lib\WebthumbnailHttpCall;
 use Webthumbnail\Lib\WebthumbnailException;
 
-    
+
 class Webthumbnail
 {
     
